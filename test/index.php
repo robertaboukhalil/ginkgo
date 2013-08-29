a119fa1c60157d60aa00f2ecea3b6a43320a8e55
robertaboukhalil <robert.aboukhalil@yahoo.com>
2013-08-29 03:11:55 -0400

    Updated dashboard

--- test/index.php
+++ test/index.php
@@ -1,4 +1,17 @@
-<!DOCTYPE html>
+<?php
+
+// Configuration
+error_reporting(E_ALL);
+session_start();
+
+// Get user's query
+$query = $_GET['q'];
+if($query == "dashboard")
+    define('SHOW_DASHBOARD', true);
+else
+    define('SHOW_DASHBOARD', false);
+
+?><!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -47,10 +60,11 @@
             <li><a href="javascript:void(0);">FAQ</a></li>
           </ul>
 
+          <?php if(SHOW_DASHBOARD): ?>
           <ul class="nav navbar-nav navbar-right">
-           <li class="dropdown" id="menu1">
-             <a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">
-               Access your results from anywhere
+           <li class="dropdown" id="menu">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#menu">
+               Access your results later
                 <b class="caret"></b>
              </a>
              <div class="dropdown-menu">
@@ -60,6 +74,7 @@
              </div>
            </li>
           </ul>
+          <?php endif; ?>
           
         </div><!--/.navbar-collapse -->
       </div>
@@ -69,22 +84,62 @@
     <div class="jumbotron">
       <div class="container">
         <h1>Ginkgo</h1>
-        <p style="margin-top:20px;">A web tool for analyzing single-cell sequencing data.</p>
+        <?php if(SHOW_DASHBOARD): ?>
+          <p style="margin-top:20px;">
+            Processing...<br />
+            <div class="progress progress-striped"><div class="progress-bar" role="progressbar" style="width: 45%"></div></div>
+          </p>
+        <?php else: ?>
+          <p style="margin-top:20px;">A web tool for analyzing single-cell sequencing data.</p>
+        <?php endif; ?>
       </div>
     </div>
 
+    <!-- Main content -->
     <div class="container">
-      <!-- Example row of columns -->
+    <?php if(SHOW_DASHBOARD): ?>
       <div class="row">
         <div class="col-lg-8">
-          <h3><span class="badge">STEP 1</span> Upload your .bed files</h3>
-          <p></p>
-          <p><a class="btn btn-lg btn-primary" href=""><span class="glyphicon glyphicon-upload"></span> Upload </a></p>
+          <h3 style="margin-top:-5px;"><span class="badge">STEP 2</span> Choose cells for analysis</h3>
+          <p>.</p>
+
+          <hr>
+
+          <h3 style="margin-top:-5px;"><span class="badge">STEP 3</span> Set analysis parameters</h3>
+          <p>.</p>
+
+          <hr>
+
+          <h3 style="margin-top:-5px;"><span class="badge">STEP 4</span> Choose e-mail options</h3>
+          <p>.</p>
+
+          <p><a class="btn btn-lg btn-primary" href="">Start Analysis <span class="glyphicon glyphicon-chevron-right"></span></a></p>
         </div>
         <div class="col-lg-4">
-          <h3><span class="badge">?</span> Help</h3>
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">Upload a new file</h3>
+            </div>
+            <div class="panel-body">
+              Panel content
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    <?php else: ?>
+      <div class="row">
+        <div class="col-lg-8">
+          <h3 style="margin-top:-5px;"><span class="badge">STEP 1</span> Upload your .bed files</h3>
+          <p></p>
+          <p><a class="btn btn-lg btn-primary" href="?q=dashboard"><span class="glyphicon glyphicon-upload"></span> Upload </a></p>
+        </div>
+        <div class="col-lg-4">
+          <h3 style="margin-top:-5px;"><span class="badge">?</span> Help</h3>
           <h4>How to make .bed files</h4>
-          <p>Open a terminal to the directory containing your files:</p>
+          <p>Open a terminal and navigate to your data folder:</p>
           <p><code>$ echo "hello";</code></p>
           <p><code>$ echo "world";</code></p>
 
@@ -93,34 +148,18 @@
           <p>
               <table class="table">
                 <thead>
-                  <tr>
-                    <th>chrom</th>
-                    <th>chromStart</th>
-                    <th>chromEnd</th>
-                  </tr>
+                  <tr><th>chrom</th><th>chromStart</th><th>chromEnd</th></tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>chr1</td>
-                    <td>555485</td>
-                    <td>555533</td>
-                  </tr>
-                  <tr>
-                    <td>chr1</td>
-                    <td>676584</td>
-                    <td>676632</td>
-                  </tr>
-                  <tr>
-                    <td>chr1</td>
-                    <td>745136</td>
-                    <td>745184</td>
-                  </tr>
+                  <tr><td>chr1</td><td>555485</td><td>555533</td></tr>
+                  <tr><td>chr1</td><td>676584</td><td>676632</td></tr>
+                  <tr><td>chr1</td><td>745136</td><td>745184</td></tr>
                 </tbody>
               </table>
           </p>
         </div>
       </div>
-
+    <?php endif; ?>
     </div> <!-- /container -->
 
 
